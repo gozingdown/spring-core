3dea58892323ece5609bb59bdf08e2493db94c3a
gozingdown <gongzhenggz@gmail.com>
2017-08-16 14:32:03 -0400

    dependency injection demo

---
+++ src/main/java/com/zheng/spring/maven/springdemo/DrawingApp.java
@@ -0,0 +1,17 @@
+package com.zheng.spring.maven.springdemo;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApp {
+	public static void main(String[] args) {
+		//Triangle triangle = new Triangle();
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle)context.getBean("triangle-alias");
+		triangle.draw();
+	}
+}
