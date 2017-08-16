2f8d80174a360a3ea39769205cea04e7ea26ade9
gozingdown <gongzhenggz@gmail.com>
2017-08-16 16:22:32 -0400

    bean autowiring

--- src/main/java/com/zheng/spring/maven/springdemo/Triangle.java
+++ src/main/java/com/zheng/spring/maven/springdemo/Triangle.java
@@ -4,20 +4,37 @@ import java.util.List;
 
 public class Triangle {
 
-	private List<Point> points;
-	
-	public List<Point> getPoints() {
-		return points;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 	public void draw() {
-		for (Point point : this.points) {
-			System.out.println("Point = (" + point.getX() + ", " + point.getY() + ")");
-		}
-		
+		System.out.println("Point A= (" + pointA.getX() + ", " + pointA.getY() + ")");
+		System.out.println("Point B= (" + pointB.getX() + ", " + pointB.getY() + ")");
+		System.out.println("Point C= (" + pointC.getX() + ", " + pointC.getY() + ")");
 	}
 }
