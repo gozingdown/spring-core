14eadc20a5f23e50ea8215d291de8d5fbaf96e88
gozingdown <gongzhenggz@gmail.com>
2017-08-17 14:17:28 -0400

    bean inheritance for lists

--- src/main/java/com/zheng/spring/maven/springdemo/Triangle.java
+++ src/main/java/com/zheng/spring/maven/springdemo/Triangle.java
@@ -1,5 +1,7 @@
 package com.zheng.spring.maven.springdemo;
 
+import java.util.List;
+
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ApplicationContext;
@@ -7,39 +9,22 @@ import org.springframework.context.ApplicationContextAware;
 
 public class Triangle implements ApplicationContextAware, BeanNameAware {
 
-	private Point pointA;
-	private Point pointB;
-	private Point pointC;
+	private List<Point> points;
 	private ApplicationContext context = null;
 
-	public Point getPointA() {
-		return pointA;
+
+	public List<Point> getPoints() {
+		return points;
 	}
 
-	public void setPointA(Point pointA) {
-		this.pointA = pointA;
-	}
-
-	public Point getPointB() {
-		return pointB;
-	}
-
-	public void setPointB(Point pointB) {
-		this.pointB = pointB;
-	}
-
-	public Point getPointC() {
-		return pointC;
-	}
-
-	public void setPointC(Point pointC) {
-		this.pointC = pointC;
+	public void setPoints(List<Point> points) {
+		this.points = points;
 	}
 
 	public void draw() {
-		System.out.println("Point A= (" + pointA.getX() + ", " + pointA.getY() + ")");
-		System.out.println("Point B= (" + pointB.getX() + ", " + pointB.getY() + ")");
-		System.out.println("Point C= (" + pointC.getX() + ", " + pointC.getY() + ")");
+		for(Point point : points) {
+			System.out.println("Point = (" + point.getX() + ", " + point.getY() + ")");
+		}
 	}
 
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
