f05e0051c03bc006f13544129e6e027fa567dcfd
gozingdown <gongzhenggz@gmail.com>
2017-08-16 23:08:05 -0400

    singleton in spring is only singleton per container(applicationcontext

--- src/main/java/com/zheng/spring/maven/springdemo/DrawingApp.java
+++ src/main/java/com/zheng/spring/maven/springdemo/DrawingApp.java
@@ -11,7 +11,13 @@ public class DrawingApp {
 		//Triangle triangle = new Triangle();
 		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		ApplicationContext context2 = new ClassPathXmlApplicationContext("spring.xml");
 		Triangle triangle = (Triangle)context.getBean("triangle");
+		triangle.draw();
+		Point pointD = (Point)context.getBean("pointD");
+		Triangle triangle2 = (Triangle)context2.getBean("triangle");
+		triangle2.setPointB(pointD);
+		triangle2.draw();
 		triangle.draw();
 	}
 }
