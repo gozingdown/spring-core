61ae1cadd7ae414671be82760febd8c6813d7e8e
Zheng Gong <zgong@vistaprint.com>
2017-08-18 16:10:45 -0400

    interface

--- src/main/java/com/zheng/spring/maven/springdemo/DrawingApp.java
+++ src/main/java/com/zheng/spring/maven/springdemo/DrawingApp.java
@@ -9,7 +9,7 @@ public class DrawingApp {
 		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		context.registerShutdownHook();
-		Shape shape = (Triangle)context.getBean("triangle1");
+		Shape shape = (Shape)context.getBean("triangle1");
 		shape.draw();
 	}
 }
