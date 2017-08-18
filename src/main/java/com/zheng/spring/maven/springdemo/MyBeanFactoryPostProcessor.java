94a4db14a120aefd01b2882f29b8dfc3cf3d8c91
Zheng Gong <zgong@vistaprint.com>
2017-08-18 15:53:28 -0400

    bean factory post processor - propertyPlaceholderConfigurer

--- src/main/java/com/zheng/spring/maven/springdemo/MyBeanFactoryPostProcessor.java
+++ src/main/java/com/zheng/spring/maven/springdemo/MyBeanFactoryPostProcessor.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
 public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 
+	// this is called before factory is instantiated.
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
 		System.out.println("My bean factory post processor is called");		
 	}
