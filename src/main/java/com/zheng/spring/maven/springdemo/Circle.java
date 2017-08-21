6b838d9727291c326b29418bcd2f01adcfb9d7aa
Zheng Gong <zgong@vistaprint.com>
2017-08-21 17:30:49 -0400

    @Component and stereotype

--- src/main/java/com/zheng/spring/maven/springdemo/Circle.java
+++ src/main/java/com/zheng/spring/maven/springdemo/Circle.java
@@ -7,7 +7,9 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Circle implements Shape{
 
 	private Point center;
